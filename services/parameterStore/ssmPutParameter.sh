fc4589155bffc4055f6e25a8ec49016d51552346
Teddy Andersson <teddyandersson95@gmail.com>
2020-03-02 14:44:21 +0100

    bash script for creating ssm env parameters for all services

---
+++ services/parameterStore/ssmPutParameter.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -eu
+
+# (C) Helsingborg Stad
+
+FILES=./envs/*
+
+# Confirm that we have the AWS cli
+if ! [ -x "$(command -v "aws")" ]; then
+  echo "Error: The aws-cli is not on the path. Perhaps it is not installed?"
+  exit 1
+fi
+
+for f in $FILES
+do
+  echo "Processing $f file..."
+  NAME=$(basename $f .json)
+  # take action on each file. $f store current file name
+  VALUE="`cat $f`"
+
+  echo "Creating SSM paramter $NAME with value: $VALUE"
+
+  aws ssm put-parameter    \
+    --type SecureString    \
+    --name "/$NAME/dev"    \
+    --value "$VALUE"
+done
