b59481b3d6c323397010055102b1fe7f5da990b0
Dan Nilsson <mail@dannilsson.se>
2020-04-20 21:02:45 +0200

    add option stage with some docs

--- services/parameterStore/ssmPutParameter.sh
+++ services/parameterStore/ssmPutParameter.sh
@@ -2,25 +2,40 @@
 
 # (C) Helsingborg Stad
 
-FILES=./envs/"[^example.]*"
-STAGE=$1
-
-echo $FILES
-
 # Confirm that we have the AWS cli
 if ! [ -x "$(command -v "aws")" ]; then
   echo "Error: The aws-cli is not on the path. Perhaps it is not installed?"
   exit 1
 fi
 
+FILES=./envs/"[^example.]*"
+
+usage() {
+  echo "usage: ssmPutParameter.sh [[-s stage] | [-h]]"
+}
+
+while [ "$1" != "" ]; do
+  case $1 in
+    -s | --stage )  shift
+                    STAGE=$1
+                    ;;
+    -h | --help )   usage
+                    exit
+                    ;;
+    * )             usage
+                    exit 1
+  esac
+  shift
+done
+
 if [ -z $STAGE ]; then
-  echo "Missing stage name"
+  echo "Error: Missing required option -s|--stage [stage]"
   exit 1
 fi
 
 for f in $FILES
 do
-  echo "Processing $f file..."
+  echo "Processing $f..."
   NAME=$(basename $f .json)
   # take action on each file. $f store current file name
   VALUE="`cat $f`"
